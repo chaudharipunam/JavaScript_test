81668c04c7261aa3571c1a1559aed4e675911900
chaudharipunam <punamchaudhari128@gmail.com>
2024-07-01 03:30:11 -0700

    test

--- test.js
+++ test.js
@@ -46,7 +46,8 @@
 // determine if the input string is valid.
 
 
-// Given a string, find the length of the longest substring without repeating characters.
+// Given an integer array, return all possible subsets (the power set).
+
 // function lengthLargestSubstring(arr){
 //     let Length = 2** arr.length;
 //     console.log(Length);
@@ -56,13 +57,44 @@
 
 
 // Given an array 'nums' of n integers where n > 1, return an array output such that output[i] is equal to the product of all the elements of nums except nums[i].
-function produAllExcOneIndex(array){
-    let product=1;
-     for(let i=0; i<array.length; i++){
-         for(let j=i; j+1<array.length; j++){
-            product*=array[j+1];
-         }
-     }
-}
+// function produAllExcOneIndex(array){
+//     let a=[];
+//     p=0;
+//      for(let i=0; i<array.length; i++){
+//         let product=1;
+//          for(let j=0; j<array.length; j++){
+//             if(j!=i){
+//               product*=array[j];
+//             }
+//          }
+//          a[p]=product;
+//          p++;
+//      }
+//      for(let k=0; k<p; k++){
+//         console.log(a[k]);
+//      }
+// }
+// produAllExcOneIndex([1,7,3,2]);
 
-produAllExcOneIndex([1,7,3,2]);
+
+
+
+function kthLargestEle(arr, kthNo) {
+    // Bubble sort to sort the array in ascending order
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    let index = arr.length - kthNo;
+    if (index >= 0 && index < arr.length) {
+        console.log(arr[index]);
+    } else {
+        console.log("Invalid k-th number");
+    }
+}
+kthLargestEle([23, 45, 21, 78, 7, 99, 47],5);
